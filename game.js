85b9ffa71495d91da25eb07ad6f77db75cde059e
rayGreycloud <rayGreycloud@gmail.com>
2017-07-03 10:18:39 -0700

    Preload images and body physics data

--- game.js
+++ game.js
@@ -23,4 +23,16 @@ let showBodies = false;
 let current time = 0;
 let gameDuration = 20;
 
- 
+// Preload
+function preload() {
+  // set background color
+  game.stage.backgroundColor = '#eee';
+
+  // Load assets
+  game.load.image('catcher', 'assets/semicircle.png');
+  game.load.image('triangle', 'assets/triangle.png');
+
+  // Load physics body polygon data
+  this.game.load.physics('physics', 'data/data.json');
+
+}
