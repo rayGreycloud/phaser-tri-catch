5e7d201d157ce0b25743f08e8e87f016d632c0f9
rayGreycloud <rayGreycloud@gmail.com>
2017-07-03 11:14:26 -0700

    Refactor variable initialization and game config

--- js/game.js
+++ js/game.js
@@ -1,26 +1,24 @@
 // game.js
 // Set game parameters
-let config = {
-  width: 800,
-  height: 500,
-  renderer: 'Phaser.AUTO',
-  parent: 'gameDiv',
-  state: {
+let width = 800;
+let height = 500;
+let renderer = 'Phaser.AUTO';
+let parent = 'gameDiv';
+let state = {
     preload,
     create,
     update
-  }
-}
+  };
 
 // Create game
-let game = new Phaser.Game(config);
+let game = new Phaser.Game(width, height, renderer, parent, state);
 
 // Initialize app variables
 let catcher, triangles, timer;
 let catcherSpeed = 250;
 let maxTriangleSpeed = 200;
 let showBodies = false;
-let current time = 0;
+let currentTime = 0;
 let gameDuration = 20;
 
 // Preload state
